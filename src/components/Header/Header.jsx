d74758ee056b50bb747bb884fd74b0cbd9e68cb1
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-04-06 22:19:32 +0600

    banglar-tshirt repository initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/review'>Order Review</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+                <li>
+                    <Link to='/contact'>Contact</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Header;
